f3a54b97cfb5ac2c601fe66175bee1436b1b5fd8
Laura Walters <laura.walters1098@yahoo.com>
2022-02-03 18:31:58 +0000

    adding number val to chart

--- js/main.js
+++ js/main.js
@@ -1,33 +1,45 @@
 // submit button event listner
-$('#user-form').on('submit', generateUserSummary);
+$("#user-form").on("submit", generateUserSummary);
 
 // generate user summary
 function generateUserSummary(event) {
   event.preventDefault();
-  console.log('submitted form');
+  console.log("submitted form");
 
-  var age = $('#age').val();
+  // assigning user values
+  var age = $("#age").val();
   console.log(age);
-  var gender = $('#gender').val();
+  var gender = $("#gender").val();
   console.log(gender);
-  var height = $('#height').val();
+  var height = $("#height").val();
   console.log(height);
-  var weight = $('#weight').val();
+  var weight = $("#weight").val();
   console.log(weight);
-  var userGoal = $('#goal').val();
+  var userGoal = $("#goal").val();
   console.log(userGoal);
-  var activitylevel = $('#activity-level').val();
+  var activitylevel = $("#activity-level").val();
   console.log(activitylevel);
 
   // fetching fitness API data
   fetch(
     // "https://fitness-calculator.p.rapidapi.com/macrocalculator?age=26&gender=male&height=180&weight=70&activitylevel=4&goal=maintain",
-    'https://fitness-calculator.p.rapidapi.com/macrocalculator?age=' + age + '&gender=' + gender + '&height=' + height + '&weight=' + weight + '&activitylevel=' + activitylevel + '&goal=' + userGoal,
+    "https://fitness-calculator.p.rapidapi.com/macrocalculator?age=" +
+      age +
+      "&gender=" +
+      gender +
+      "&height=" +
+      height +
+      "&weight=" +
+      weight +
+      "&activitylevel=" +
+      activitylevel +
+      "&goal=" +
+      userGoal,
     {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'x-rapidapi-host': 'fitness-calculator.p.rapidapi.com',
-        'x-rapidapi-key': '2a61fe40cdmshb25b8249e993d82p1d55cejsn5658af22a824',
+        "x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
+        "x-rapidapi-key": "2a61fe40cdmshb25b8249e993d82p1d55cejsn5658af22a824",
       },
     }
   )
@@ -36,6 +48,7 @@ function generateUserSummary(event) {
     })
     .then(function (data) {
       console.log(data);
+      // function that makes the users summary visible in a chart
       renderUserData(data);
     });
 }
@@ -43,36 +56,45 @@ function generateUserSummary(event) {
 function renderUserData(userData) {
   console.log(userData.data);
 
+  // Declaring variables for recommended macros
   var protein = Math.round(userData.data.balanced.protein);
   var fat = Math.round(userData.data.balanced.fat);
   var carbs = Math.round(userData.data.balanced.carbs);
+  var calories = Math.round(userData.data.calorie);
 
   var userCard = $(`
   <div class="flex justify-center">
   <h1 class="font-extrabold"> Target Calories: </h1>
-  <p>${Math.round(userData.data.calorie)}</p>
+  <p>${calories}</p>
   </div>
     `);
 
-  $('#userCard').append(userCard);
+  $("#userCard").append(userCard);
 
   const dataDoughnut = {
-    labels: ['Protein (g)', 'Fat (g)', 'Carbs (g)'],
+    labels: [`Protein ${protein}g`, `Fat ${fat}g`, `Carbs ${carbs}g`],
     datasets: [
       {
-        label: 'Nutrition',
+        label: "Nutrition",
         data: [protein, fat, carbs],
-        backgroundColor: ['rgb(133, 105, 241)', 'rgb(164, 101, 241)', 'rgb(101, 143, 241)'],
+        backgroundColor: [
+          "rgb(133, 105, 241)",
+          "rgb(164, 101, 241)",
+          "rgb(101, 143, 241)",
+        ],
         hoverOffset: 4,
       },
     ],
   };
 
   const configDoughnut = {
-    type: 'doughnut',
+    type: "doughnut",
     data: dataDoughnut,
     options: {},
   };
 
-  var chartBar = new Chart(document.getElementById('chartDoughnut'), configDoughnut);
+  var chartBar = new Chart(
+    document.getElementById("chartDoughnut"),
+    configDoughnut
+  );
 }
